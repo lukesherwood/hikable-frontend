14fae38b3948912a77d2d36afec51e438274efa1
lukesherwood <luke.sherwood55@gmail.com>
2020-12-30 08:52:26 +1300

    refactoring footer by changing to functional component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,25 @@
-import React, { Component } from "react";
+import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 
-class Footer extends Component {
-  render() {
-    return (
-      <div className="footer">
-        <Navbar className="navbar-bottom justify-content-center" bg="dark" variant="dark">
-          <Navbar.Brand href="/">
-            <img src="/Hikable-logo.png" alt="hikable-logo" width="70" />
-          </Navbar.Brand>
-          <Nav.Item>2020 Hikable, All Rights Reserved</Nav.Item>
-          <Nav.Link href="#">Privacy Policy</Nav.Link>
-          <Nav.Link href="#">Terms of Service </Nav.Link>
-          <Nav.Link href="#">Cookie Policy </Nav.Link>
-        </Navbar>
-      </div>
-    );
-  }
+function Footer() {
+  return (
+    <div className="footer">
+      <Navbar
+        className="navbar-bottom justify-content-center"
+        bg="dark"
+        variant="dark"
+      >
+        <Navbar.Brand href="/">
+          <img src="/Hikable-logo.png" alt="hikable-logo" width="70" />
+        </Navbar.Brand>
+        <Nav.Item>2020 Hikable, All Rights Reserved</Nav.Item>
+        <Nav.Link href="#">Privacy Policy</Nav.Link>
+        <Nav.Link href="#">Terms of Service </Nav.Link>
+        <Nav.Link href="#">Cookie Policy </Nav.Link>
+      </Navbar>
+    </div>
+  );
 }
 
 export default Footer;
