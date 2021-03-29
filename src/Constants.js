1de3f4bab6d4c74b1ef8e9bb8a1847bf002d40b9
lukesherwood <luke.sherwood55@gmail.com>
2021-03-29 13:13:20 -0400

    changed url constants

--- src/Constants.js
+++ src/Constants.js
@@ -1,11 +1,11 @@
 const prod = {
   url: {
-    API_URL: 'http://hikable.herokuapp.com/api/v1/',
+    API_URL: 'http://hikable.herokuapp.com/api/v1',
   },
 };
 const dev = {
   url: {
-    API_URL: `http://localhost:3001/api/v1/`,
+    API_URL: `http://localhost:3001/api/v1`,
   },
 };
 export const config = process.env.NODE_ENV === `development` ? dev : prod;
