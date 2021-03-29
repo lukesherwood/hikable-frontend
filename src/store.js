d184000cad57ac22c46e239ffda3175ddee76697
lukesherwood <luke.sherwood55@gmail.com>
2021-03-29 13:17:35 -0400

    fixed devtools issue

--- src/store.js
+++ src/store.js
@@ -17,12 +17,12 @@ import {
     users
   });
   
-  const middleware = [thunk];
-  
-  export default createStore(
-    reducers,
-    compose(
-      applyMiddleware(...middleware),
+  let middleware = [thunk];
+  let enhancers = [applyMiddleware(...middleware)];
+  if (process.env.NODE_ENV === "development") {
+    enhancers.push(
       window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-  );
+    );
+  }
+  
+  export default createStore(reducers, compose(...enhancers));
