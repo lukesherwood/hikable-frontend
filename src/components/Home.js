7785b061a2e05b9b5ef8b2f7492e88a81b115959
lukesherwood <luke.sherwood55@gmail.com>
2021-04-12 17:53:35 -0400

    homepage responsiveness improved

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,28 +5,21 @@ export default function Home() {
     <div className="home">
       <div className="home-image">
         {/* image here with css */}
-        <br></br>
-        <br></br>
-        <div className="home-header">
+        <div className="home-header p-4">
           <p>Your favorite hike is just around the corner.</p>
         </div>
-        <br></br>
-        <br></br>
-        <div className="home-feature">
+        <div className="home-feature p-3">
           <p>Find a hike. </p>
           <p>Create a list. </p>
           <p>Add hikes to your list. </p>
           <p>Get hiking!</p>
           <p>Truly Hikable!</p>
         </div>
-        <br></br>
-        <br></br>
-        <div className="home-link">
+        <div className="home-link p-3">
           <Link to="/hikes" className="btn btn-custom btn-lg" role="button">
             Click here to get started
           </Link>
         </div>
-        <br></br>
       </div>
     </div>
   );
