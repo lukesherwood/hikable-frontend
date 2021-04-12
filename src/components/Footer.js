8e3a26857dbb181f2e65bbe36d9e79cf5e8716f0
lukesherwood <luke.sherwood55@gmail.com>
2021-04-12 16:59:47 -0400

    fixed responsiveness of footer nav

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,18 +6,25 @@ import Nav from "react-bootstrap/Nav";
 export default function Footer() {
   return (
     <div className="footer">
-      <Navbar
-        className="navbar-bottom justify-content-center"
-        bg="dark"
-        variant="dark"
-      >
-        <Navbar.Brand as={NavLink} to="/">
-          <img src="/Hikable-logo.png" alt="hikable-logo" width="70" />
-        </Navbar.Brand>
-        <Nav.Item>2020 Hikable, All Rights Reserved</Nav.Item>
-        <Nav.Link href="#">Privacy Policy</Nav.Link>
-        <Nav.Link href="#">Terms of Service </Nav.Link>
-        <Nav.Link href="#">Cookie Policy </Nav.Link>
+      <Navbar collapseOnSelect expand="md" className="navbar-bottom" bg="dark" variant="dark">
+        <Navbar.Toggle aria-controls="footer-navbar-nav" />
+        <Navbar.Collapse id="footer-navbar-nav">
+          <Nav className="justify-content-center w-100">
+            <Nav.Item className="pt-4 pr-2">
+              2020 Hikable, All Rights Reserved
+            </Nav.Item>
+            <Navbar.Brand
+              className="justify-content-center"
+              as={NavLink}
+              to="/"
+            >
+              <img src="/Hikable-logo.png" alt="hikable-logo" width="70" />
+            </Navbar.Brand>
+            <Nav.Link href="">Privacy Policy</Nav.Link>
+            <Nav.Link href="">Terms of Service </Nav.Link>
+            <Nav.Link href="">Cookie Policy </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </div>
   );
